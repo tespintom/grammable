af97e53b0f53f22c58c3cb14b179d6fc979ee8a2
Tomas Espina <tespintom@icloud.com>
2017-10-23 22:33:49 +0000

    typo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,4 @@
 # config/initializers/carrierwave.rb
-
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
@@ -7,5 +6,5 @@ CarrierWave.configure do |config|
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+  config.fog_directory = ENV["AWS_BUCKET"]               # required
 end
