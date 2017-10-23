814490528c1566ff1d1740e689e4fc1ced69c164
Tomas Espina <tespintom@icloud.com>
2017-10-23 17:31:25 +0000

    added gram detail page

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,18 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
-  describe "grams#index action" do
-    it "should successfully show the page" do
+  describe 'grams#show action' do
+    it 'should successfully show the page if the gram is found' do
+      gram = FactoryBot.create(:gram)
+      get :show, params: { id: gram.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'should return a 404 error if th gram is not found' do
+      get :show, params: { id: 'TACOCAT' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+  describe 'grams#index action' do
+    it 'should successfully show the page' do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
-  describe "grams#new action" do
-    it "should require users to be logged in" do
+  describe 'grams#new action' do
+    it 'should require users to be logged in' do
       get :new
       expect(response).to redirect_to new_user_session_path
     end
-    it "should successfully show the new form" do
+    it 'should successfully show the new form' do
       user = FactoryBot.create(:user)
       sign_in user
 
@@ -21,22 +33,22 @@ RSpec.describe GramsController, type: :controller do
     end
   end
 
-  describe "grams#create action" do
-    it "should require users to be logged in" do
+  describe 'grams#create action' do
+    it 'should require users to be logged in' do
       post :create, params: { gram: { message: 'Hello' } }
     end
-    it "should successfully create a new gram in our database" do
+    it 'should successfully create a new gram in our database' do
       user = FactoryBot.create(:user)
       sign_in user
 
-      post :create, params: { gram: {message: 'Hello!'} }
+      post :create, params: { gram: { message: 'Hello!' } }
       expect(response).to redirect_to root_path
 
       gram = Gram.last
       expect(gram.message).to eq('Hello!')
       expect(gram.user).to eq(user)
     end
-    it "should properly deal with validation erros" do
+    it 'should properly deal with validation erros' do
       user = FactoryBot.create(:user)
       sign_in user
       post :create, params: { gram: {message: '' } }
